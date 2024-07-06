4ad5fef6e9ff6284abbf73dcf022c4afe87b8a1a
Emre Özdin <mrzdnboun@gmail.com>
2024-07-06 18:19:33 +0300

    "Remove react-native imports and Text component, add Redirect from expo-router"

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
-import { View, Text } from 'react-native'
+import { Redirect } from "expo-router";
 
 export default function Home() {
-  return (
-      <Text>Hello World</Text>)
+  return <Redirect href="/(tabs)" />;
 }
