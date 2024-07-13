e4b40b77065cad43d62af5a5f6a6367a5fe263a8
Emre Özdin <mrzdnboun@gmail.com>
2024-07-13 20:13:34 +0300

    "Updated FeedScreen component to return a View with styled Text instead of a plain Text"

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 export default function FeedScreen() {
-  return <Text>Feed</Text>;
+  return (
+    <View className="bg-slate-600 flex-1 items-center justify-center m-10">
+      <Text className="text-md md:text-5xl font-bold text-blue-500 md: text-red-500">
+        Feed
+      </Text>
+    </View>
+  );
 }
