7ec078629539544e53e81f6dd344c6ca57026ace
Emre Özdin <mrzdnboun@gmail.com>
2024-07-29 18:35:00 +0300

    posts.json added

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
 import { Text, View } from "react-native";
+import posts from "~/assets/data/posts.json";
+
+const post1 = posts[0];
 export default function FeedScreen() {
+  console.log(post1);
   return (
     <View className="bg-slate-600 flex-1 items-center justify-center m-10">
       <Text className="text-md md:text-5xl font-bold text-blue-500 md: text-red-500">
