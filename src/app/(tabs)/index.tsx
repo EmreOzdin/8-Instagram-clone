4365a4c66817824ca869f43cd8c0ac82869141d1
Emre Özdin <mrzdnboun@gmail.com>
2024-07-31 13:02:22 +0300

    "Removed console log and post1 variable, added className prop to FlatList, and updated contentContainerStyle"

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,15 +2,15 @@ import { FlatList } from "react-native";
 import posts from "~/assets/data/posts.json";
 import PostListItem from "~/src/components/PostListItem";
 
-const post1 = posts[0];
+
 export default function FeedScreen() {
-  console.log(post1);
   return (
     <FlatList
       data={posts}
+      className=""
       renderItem={({ item }) => <PostListItem post={item} />}
       keyExtractor={(post) => post.id}
-      contentContainerStyle={{ gap: 10 }}
+      contentContainerStyle={{ gap: 10, maxWidth: 512, width: "100%" }}
       showsVerticalScrollIndicator={false}
     />
   );
