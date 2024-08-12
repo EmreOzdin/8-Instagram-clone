c98b3fd6212c8a4ba263a957f920ec18e8cbddc0
Emre Özdin <mrzdnboun@gmail.com>
2024-08-12 19:07:29 +0300

    "Updated CreatePost component to support video uploads, added cloudinary upload functionality, and refactored code organization"

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,58 +1,93 @@
-import { Image, Text, View, TextInput } from "react-native";
+import { Text, View, Image, TextInput, Pressable } from "react-native";
 import { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import Button from "~/src/components/Button";
+import { uploadImage } from "~/src/lib/cloudinary";
+import { supabase } from "~/src/lib/supabase";
+import { useAuth } from "~/src/providers/AuthProvider";
+import { router } from "expo-router";
+import { ResizeMode, Video } from "expo-av";
 
 export default function CreatePost() {
   const [caption, setCaption] = useState("");
-  const [image, setImage] = useState<string | null>(null);
+  const [media, setMedia] = useState<string | null>(null);
+  const [mediaType, setMediaType] = useState<"video" | "image" | undefined>();
+
+  const { session } = useAuth();
 
   useEffect(() => {
-    if (!image) {
-      pickImage();
+    if (!media) {
+      pickMedia();
     }
-  }, [image]);
+  }, [media]);
 
-  const pickImage = async () => {
+  const pickMedia = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
-      quality: 1,
+      quality: 0.5,
     });
 
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      setMedia(result.assets[0].uri);
+      setMediaType(result.assets[0].type);
     }
   };
 
-  const uploadImage = async () => {
-    //upload the image to cloudinary
-    //return the cloudinary image url
-  };
-
   const createPost = async () => {
-    await uploadImage();
-    //Save the post in database
+    if (!media) {
+      return;
+    }
+    const response = await uploadImage(media);
+    // Save the post in database
+    console.log("image id: ", response?.public_id);
+
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([
+        {
+          caption,
+          image: response?.public_id,
+          user_id: session?.user.id,
+          media_type: mediaType,
+        },
+      ])
+      .select();
+
+    router.push("/(tabs)");
   };
 
   return (
     <View className="p-3 items-center flex-1">
-      {/*Image Picker*/}
-      {image ? (
+      {/* Image picker */}
+      {!media ? (
+        <View className="w-52 aspect-[3/4] rounded-lg bg-slate-300" />
+      ) : mediaType === "image" ? (
         <Image
-          source={{ uri: image }}
-          className="w-52 aspect-[3/4] rounded-lg shadow-lg"
+          source={{ uri: media }}
+          className="w-52 aspect-[3/4] rounded-lg bg-slate-300"
         />
       ) : (
-        <View className="w-52 aspect-[3/4] rounded-lg shadow-lg" />
+        <Video
+          className="w-52 aspect-[3/4] rounded-lg bg-slate-300"
+          style={{ width: "100%", aspectRatio: 16 / 9 }}
+          source={{
+            uri: media,
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          shouldPlay
+        />
       )}
-      <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
+
+      <Text onPress={pickMedia} className="text-blue-500 font-semibold m-5">
         Change
       </Text>
 
-      {/*Text Input forCaption*/}
+      {/* TextInput for caption */}
       <TextInput
         value={caption}
         onChangeText={(newValue) => setCaption(newValue)}
@@ -60,8 +95,8 @@ export default function CreatePost() {
         className="w-full p-3"
       />
 
-      {/*Button*/}
-      <View className="mt-24 w-full">
+      {/* Button */}
+      <View className="mt-auto w-full">
         <Button title="Share" onPress={createPost} />
       </View>
     </View>
