36b74138c36027fa5c8a646335e7036e8d50321e
Emre Özdin <mrzdnboun@gmail.com>
2024-08-13 12:15:28 +0300

    last update

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,25 @@
-import { FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, FlatList } from "react-native";
 import posts from "~/assets/data/posts.json";
 import PostListItem from "~/src/components/PostListItem";
+import { supabase } from "~/src/lib/supabase";
 
 export default function FeedScreen() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    let { data: posts, error } = await supabase.from("posts").select("*");
+    if (error) {
+      Alert.alert("Something went wrong");
+    }
+  };
+
+  console.log(posts);
+
   return (
     <FlatList
       data={posts}
